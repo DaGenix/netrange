9b73ad7b32b36542e8590240f9112c49890d2ffd
Palmer Cox <palmer@fiscalnote.com>
2021-03-10 21:43:04 -0500

    wip

---
+++ libnetrangemerge/src/bin/run_merge.rs
@@ -0,0 +1,20 @@
+use libnetrangemerge::{merge_networks, NetworkInterest, Ipv4Network};
+
+fn simple_ipv4_data() -> Vec<NetworkInterest<Ipv4Network>> {
+    vec![
+        NetworkInterest::new("127.0.0.0/31".parse().unwrap(), false),
+        NetworkInterest::new("127.0.0.2/31".parse().unwrap(), true),
+        NetworkInterest::new("127.0.0.0/30".parse().unwrap(), true),
+        NetworkInterest::new("127.0.0.4/30".parse().unwrap(), true),
+        NetworkInterest::new("127.0.0.8/31".parse().unwrap(), true),
+        NetworkInterest::new("127.0.0.10/31".parse().unwrap(), true),
+        NetworkInterest::new("127.0.4.0/23".parse().unwrap(), true),
+        NetworkInterest::new("127.0.6.0/23".parse().unwrap(), true),
+    ]
+}
+
+fn main() {
+    for _ in 0..1000 {
+        merge_networks(&mut simple_ipv4_data());
+    }
+}
